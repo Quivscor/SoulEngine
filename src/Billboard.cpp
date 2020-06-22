b0ad6c895d443d4d74d045a4c0f39cc7c0211b1a
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2020-06-22 23:23:53 +0200

    rgba

--- src/Billboard.cpp
+++ src/Billboard.cpp
@@ -49,7 +49,7 @@ Billboard::Billboard(char* imagepath, bool x)
 		unsigned char* data = stbi_load(imagepath, &width, &height, &nrChannels, 0);
 		if (data)
 		{
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 			glGenerateMipmap(GL_TEXTURE_2D);
 		}
 		stbi_image_free(data);
